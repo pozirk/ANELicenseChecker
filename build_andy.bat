a69a05e4d22c1163fcd133ce49055e9cecebd88e
pozirk <pozirk@gmail.com>
2013-05-15 15:18:42 -0400

    default platform (didn't test)

--- build_andy.bat
+++ build_andy.bat
@@ -8,4 +8,7 @@ rd android\LicenseChecker\bin\classes /S /Q
 
 "c:\Program Files (x86)\Java\jdk1.7.0_13\bin\jar.exe" cvf android/LicenseChecker/build/libLicenseChecker.jar -C android/LicenseChecker/bin .
 
-"c:\Program Files (x86)\Adobe\AIR3.7\bin\adt.bat" -package -target ane LicenseChecker.ane air/extension.xml -swc air/LicenseChecker/bin/LicenseChecker.swc -platform Android-ARM -C android/LicenseChecker/build/ .
+SET PLATFORM_ANDROID= -platform Android-ARM -C android\LicenseChecker\build\ .
+SET PLATFORM_DEFAULT= -platform default -C default\ .
+
+"c:\Program Files (x86)\Adobe\AIR3.7\bin\adt.bat" -package -target ane LicenseChecker.ane air/extension.xml -swc air/LicenseChecker/bin/LicenseChecker.swc %PLATFORM_ANDROID% %PLATFORM_DEFAULT%
--- build_sam.bat
+++ build_sam.bat
@@ -8,4 +8,7 @@ rd android\SAMLicenseChecker\bin\classes /S /Q
 
 "c:\Program Files (x86)\Java\jdk1.7.0_13\bin\jar.exe" cvf android/SAMLicenseChecker/build/libLicenseChecker.jar -C android/SAMLicenseChecker/bin .
 
-"c:\Program Files (x86)\Adobe\AIR3.7\bin\adt.bat" -package -target ane SAMLicenseChecker.ane air/extension.xml -swc air/LicenseChecker/bin/LicenseChecker.swc -platform Android-ARM -C android/SAMLicenseChecker/build/ .
+SET PLATFORM_ANDROID= -platform Android-ARM -C android\SAMLicenseChecker\build\ .
+SET PLATFORM_DEFAULT= -platform default -C default\ .
+
+"c:\Program Files (x86)\Adobe\AIR3.7\bin\adt.bat" -package -target ane SAMLicenseChecker.ane air/extension.xml -swc air/LicenseChecker/bin/LicenseChecker.swc %PLATFORM_ANDROID% %PLATFORM_DEFAULT%
